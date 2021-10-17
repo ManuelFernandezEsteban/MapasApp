74625169e40f7719092c37d0c2f138fe5faac6e1
Manuel1977 <Manuel_fe1977@hotmail.com>
2021-10-17 13:57:30 +0200

    Estructura de la aplicacion y rutas

---
+++ src/app/shared/menu/menu.component.ts
@@ -0,0 +1,41 @@
+import { Component, OnInit } from '@angular/core';
+
+interface MenuItem{
+  nombre:string;
+  ruta:string;
+}
+
+@Component({
+  selector: 'app-menu',
+  templateUrl: './menu.component.html',
+  styles: [
+    `
+    li{
+      cursor:pointer;
+    }
+    `
+  ]
+})
+export class MenuComponent {
+
+  menuItem:MenuItem[]=[
+    {
+      ruta:'/mapas/fullscreen',
+      nombre:'FullScreen'
+    },
+    {
+      ruta:'/mapas/zoom-range',
+      nombre:'ZoomRange'
+    },
+    {
+      ruta:'/mapas/propiedades',
+      nombre:'Propiedades'
+    },
+    {
+      ruta:'/mapas/marcadores',
+      nombre:'Marcadores'
+    },
+  ]
+  
+
+}
